1753cdf9e5def8671c515b1c1cabca6194e18065
Kristen <kristen_grinyer@icloud.com>
2021-11-13 12:41:09 -0500

    Checker number hints added
    updated tile prefab and my scripts

--- 5203-Chromatic-Maze/Assets/Scripts/Kristen/NumClues.cs
+++ 5203-Chromatic-Maze/Assets/Scripts/Kristen/NumClues.cs
@@ -9,40 +9,38 @@ public class NumClues : MonoBehaviour
     [HideInInspector] private static int width;
     [HideInInspector] private static int height;
 
-    void Start()
+    public static void SetClues(Tile[] tiles)
     {
         width = GenerateGrid.wdth;
         height = GenerateGrid.hght;
-    }
 
-    public static void SetClues(Tile[] tiles)
-    {
-        for(int j = 1; j <= height; j++) // j = column number
+        //Checking columns
+        for (int j = 0; j < width; j++) // j = column number
         {
             int checkers = 0;
-            for (int i = 0; i < height; i += width) //i = tile number
+            for (int i = 0; i < height; i++) //i = tile number
             {
-                if (tiles[i].tag == "checker")
+                if (tiles[j+(i*width)].tag == "checker")
                 {
                     checkers++;
                 }
             }
-            GameObject.Find("TopWall-" + j).GetComponentInChildren<Text>().text = checkers.ToString();
+            GameObject.Find("TopWall-" + (j+1)).GetComponentInChildren<Text>().text = checkers.ToString();
             //get wall prefab and set its text to checkers
         }
 
-        for (int j = 1; j <= width; j++) // j = column number
+        //Checking rows
+        for (int j = 0; j < height; j++) // j = column number
         {
             int checkers = 0;
-            for (int i = 0; i < width; i += height) //i = tile number
+            for (int i = 1; i <= width; i++) //i = tile number
             {
-                if (tiles[i].tag == "checker")
+                if (tiles[i + (j*width) - 1].tag == "checker")
                 {
                     checkers++;
                 }
             }
-            GameObject.Find("LeftWall-" + j).GetComponentInChildren<Text>().text = checkers.ToString(); ;
-            //get wall prefab and set its text to checkers
+            GameObject.Find("LeftWall-" + (j+1)).GetComponentInChildren<Text>().text = checkers.ToString(); ;
         }
     }
 
