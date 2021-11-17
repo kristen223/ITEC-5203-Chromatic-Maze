a9dbbd35aef24a7fb21651b88f650773136de468
Rifah Sama Aziz <rifahz999@gmail.com>
2021-11-17 00:59:16 -0500

    Update rules.cs

--- 5203-Chromatic-Maze/Assets/Scripts/Rifah/rules.cs
+++ 5203-Chromatic-Maze/Assets/Scripts/Rifah/rules.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,39 +9,58 @@ public struct MovementRules
     public int direction; //N,S,E,W=1111,  dont care=-1
     public int distance; //dont care = -1
     public int color; //-1 means dont care , 1=yellow, 2=orange, 3=red, 4=green, 5=blue, 6=purple
+    public int w; //assigning not done
+
+    public static implicit operator MovementRules(List<MovementRules> v)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public struct ColorRules
 {
     public bool inclusion;
     public int color;
+    public int w;//assigning not done 
+
+    public static implicit operator ColorRules(List<ColorRules> v)
+    {
+        throw new NotImplementedException();
+    }
 }
+//two lists => same length,two chromosomes, genetic algo
 
 public class Rules : MonoBehaviour
 {
-    public MovementRules Tmove;
-    public MovementRules blank;
-    public MovementRules teleportB;
-    public MovementRules teleportP;
-    public MovementRules teleportG;
-    public MovementRules teleportR;
-    public MovementRules teleportO;
-    public MovementRules teleportY;
-    public MovementRules jumpOne;
-    public MovementRules jumpTwo;
-    public MovementRules warmTemp;
-    public MovementRules coldTemp;
-    public ColorRules excludeR;
-    public ColorRules excludeO;
-    public ColorRules excludeY;
-    public ColorRules excludeB;
-    public ColorRules excludeG;
-    public ColorRules excludeP;
-    public ColorRules block;
+
+    //global variables => rules
+    private MovementRules Tmove;
+    private MovementRules blank;
+    private MovementRules teleportB;
+    private MovementRules teleportP;
+    private MovementRules teleportG;
+    private MovementRules teleportR;
+    private MovementRules teleportO;
+    private MovementRules teleportY;
+    private MovementRules jumpOne;
+    private MovementRules jumpTwo;
+    private MovementRules warmTemp;
+    private MovementRules coldTemp;
+    private ColorRules excludeR;
+    private ColorRules excludeO;
+    private ColorRules excludeY;
+    private ColorRules excludeB;
+    private ColorRules excludeG;
+    private ColorRules excludeP;
+    private ColorRules block;
+
+    public MovementRules movementRuleSets = new List<MovementRules>();
+    public ColorRules colorRuleSets = new List<ColorRules>();
 
     void start()
     {
         defineRules();
+        fitnessOne();
        
 
     }
@@ -50,63 +70,69 @@ public class Rules : MonoBehaviour
         Tmove.direction = 1011;
         Tmove.distance = 1;
         Tmove.color = -1;
+        movementRuleSets.Add(Tmove);
 
-       
+
         blank.direction = 1111;
         blank.distance = 1;
         blank.color = -1;
+        movementRuleSets.Add(blank);
 
-        
         teleportB.direction = -1;
         teleportB.distance = -1;
         teleportB.color = 5;
+        movementRuleSets.Add(teleportB);
 
-  
+
         teleportP.direction = -1;
         teleportP.distance = -1;
         teleportP.color = 6;
+        movementRuleSets.Add(teleportP);
 
-       
         teleportG.direction = -1;
         teleportG.distance = -1;
         teleportG.color = 4;
+        movementRuleSets.Add(teleportG);
 
-        
+
         teleportR.direction = -1;
         teleportR.distance = -1;
         teleportR.color = 3;
+        movementRuleSets.Add(teleportR);
 
-        
         teleportO.direction = -1;
         teleportO.distance = -1;
         teleportO.color = 2;
+        movementRuleSets.Add(teleportO);
 
-        
+
         teleportY.direction = -1;
         teleportY.distance = -1;
         teleportY.color = 1;
+        movementRuleSets.Add(teleportY);
 
-        
+
         jumpOne.direction = 1111;
         jumpOne.distance = 2;
         jumpOne.color = -1;
+        movementRuleSets.Add(jumpOne);
 
-       
+
         jumpTwo.direction = 1111;
         jumpTwo.distance = 3;
         jumpTwo.color = -1;
+        movementRuleSets.Add(jumpTwo);
 
-        
         warmTemp.direction = 1111;
         warmTemp.distance = 1;
         warmTemp.color = 1 | 2 | 3; //does this work or should it be seperate?
+        movementRuleSets.Add(warmTemp);
 
 
-       
         coldTemp.direction = 1111;
         coldTemp.distance = 1;
         coldTemp.color = 4 | 5 | 6;
-
+        movementRuleSets.Add(coldTemp);
 
         ColorRules includeY;// if blue, goes to yellow
         // int currentpos = 9999;//take from the maze/how to keep track of prevs position when maze is not built?
@@ -119,7 +145,7 @@ public class Rules : MonoBehaviour
         
         excludeR.color = 1 | 2 | 4 | 5 | 6;
         excludeR.inclusion = false;
-
+        //add to the list
    
         excludeO.color = 1 | 3 | 4 | 5 | 6;
         excludeO.inclusion = false;
@@ -143,6 +169,22 @@ public class Rules : MonoBehaviour
         ColorRules block; //block is the same as exclude C if we dont keep track of prevs position
 
     }
+
+    public void fitnessOne(List<MovementRules> m, List<ColorRules> c) 
+    {
+        int w1, w2;
+
+        for (int i = 0; i < m.Count; i++)
+        {
+            w1 = m[i].w;
+            w2 = c[i].w;
+            
+           
+        }
+
+        //fitness = [chromosome[1] for chromosome in population ]
+
+
 
 
 }
