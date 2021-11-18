5a70668fd535160cc7b7d5fe792479099be2646b
Rifah Sama Aziz <rifahz999@gmail.com>
2021-11-18 01:18:40 -0500

    Update rules.cs

--- 5203-Chromatic-Maze/Assets/Scripts/Rifah/rules.cs
+++ 5203-Chromatic-Maze/Assets/Scripts/Rifah/rules.cs
@@ -9,7 +9,7 @@ public struct MovementRules
     public int direction; //N,S,E,W=1111,  dont care=-1
     public int distance; //dont care = -1
     public int color; //-1 means dont care , 1=yellow, 2=orange, 3=red, 4=green, 5=blue, 6=purple
-    public int w; //assigning not done
+    public int type; //assigning not done
 
     public static implicit operator MovementRules(List<MovementRules> v)
     {
@@ -17,178 +17,336 @@ public struct MovementRules
     }
 }
 
-public struct ColorRules
+public struct ColorRules  
 {
     public bool inclusion;
-    public int color;
-    public int w;//assigning not done 
+    public int src;
+    public int target;
+    public int type;//assigning not done 
 
     public static implicit operator ColorRules(List<ColorRules> v)
     {
         throw new NotImplementedException();
     }
 }
-//two lists => same length,two chromosomes, genetic algo
+
+public struct RuleTypes //chromosomes
+{
+    string name;
+    int type;
+}
 
 public class Rules : MonoBehaviour
 {
 
     //global variables => rules
-    private MovementRules Tmove;
-    private MovementRules blank;
-    private MovementRules teleportB;
-    private MovementRules teleportP;
-    private MovementRules teleportG;
-    private MovementRules teleportR;
-    private MovementRules teleportO;
-    private MovementRules teleportY;
-    private MovementRules jumpOne;
-    private MovementRules jumpTwo;
-    private MovementRules warmTemp;
-    private MovementRules coldTemp;
-    private ColorRules excludeR;
-    private ColorRules excludeO;
-    private ColorRules excludeY;
-    private ColorRules excludeB;
-    private ColorRules excludeG;
-    private ColorRules excludeP;
-    private ColorRules block;
+    public MovementRules Tmove;
+    public MovementRules blank;
+    public MovementRules teleportB;
+    public MovementRules teleportP;
+    public MovementRules teleportG;
+    public MovementRules teleportR;
+    public MovementRules teleportO;
+    public MovementRules teleportY;
+    public MovementRules jumpOne;
+    public MovementRules jumpTwo;
+    public MovementRules warmTemp;
+    public MovementRules coldTemp;
+    public ColorRules includeBY; //if this is applied, other B? cant be applied.Make sure this rule is not the same as temperature. need to NOT have the temperature thing here.
+    public ColorRules includePR;//same as above
+    public ColorRules includeOG;
+    public ColorRules includeGR;
+    public ColorRules includeRB;
+    public ColorRules includeYP;//more combination possible
+                                //did not count the color 'pink' in.
 
-    public MovementRules movementRuleSets = new List<MovementRules>();
-    public ColorRules colorRuleSets = new List<ColorRules>();
+    public ColorRules excludeR;
+    public ColorRules excludeO;
+    public ColorRules excludeY;
+    public ColorRules excludeB;
+    public ColorRules excludeG;
+    public ColorRules excludeP;
+    public ColorRules blockY;
+    public ColorRules blockO;
+    public ColorRules blockR;
+    public ColorRules blockG;
+    public ColorRules blockB;
+    public ColorRules blockP;
+
+    public List<MovementRules> movementRuleSets = new List<MovementRules>();
+    public List<ColorRules> colorRuleSets = new List<ColorRules>();
 
     void start()
     {
-        defineRules();
-        fitnessOne();
        
+        defineRules();
+        crossover(movementRuleSets,colorRuleSets); //mutate and fitness are nested in crossover
 
     }
+
+  
+
     public void defineRules()
     {
         
         Tmove.direction = 1011;
         Tmove.distance = 1;
         Tmove.color = -1;
+        Tmove.type = 1;
         movementRuleSets.Add(Tmove);
 
 
         blank.direction = 1111;
         blank.distance = 1;
         blank.color = -1;
+        blank.type = 2;
         movementRuleSets.Add(blank);
+        
 
         teleportB.direction = -1;
         teleportB.distance = -1;
         teleportB.color = 5;
+        teleportB.type = 3;
         movementRuleSets.Add(teleportB);
 
 
         teleportP.direction = -1;
         teleportP.distance = -1;
         teleportP.color = 6;
+        teleportP.type = 3;
         movementRuleSets.Add(teleportP);
 
         teleportG.direction = -1;
         teleportG.distance = -1;
         teleportG.color = 4;
+        teleportG.type = 3;
         movementRuleSets.Add(teleportG);
 
 
         teleportR.direction = -1;
         teleportR.distance = -1;
         teleportR.color = 3;
+        teleportR.type = 3;
         movementRuleSets.Add(teleportR);
 
         teleportO.direction = -1;
         teleportO.distance = -1;
         teleportO.color = 2;
+        teleportO.type = 3;
         movementRuleSets.Add(teleportO);
 
 
         teleportY.direction = -1;
         teleportY.distance = -1;
         teleportY.color = 1;
+        teleportY.type = 3;
         movementRuleSets.Add(teleportY);
 
 
         jumpOne.direction = 1111;
         jumpOne.distance = 2;
         jumpOne.color = -1;
+        jumpOne.type = 4;
         movementRuleSets.Add(jumpOne);
 
 
         jumpTwo.direction = 1111;
         jumpTwo.distance = 3;
         jumpTwo.color = -1;
+        jumpTwo.type = 5;
         movementRuleSets.Add(jumpTwo);
 
         warmTemp.direction = 1111;
         warmTemp.distance = 1;
         warmTemp.color = 1 | 2 | 3; //does this work or should it be seperate?
+        warmTemp.type = 6;
         movementRuleSets.Add(warmTemp);
 
 
         coldTemp.direction = 1111;
         coldTemp.distance = 1;
         coldTemp.color = 4 | 5 | 6;
+        coldTemp.type = 7;
         movementRuleSets.Add(coldTemp);
 
-        ColorRules includeY;// if blue, goes to yellow
+        ColorRules includeBY;// if blue, goes to yellow
         // int currentpos = 9999;//take from the maze/how to keep track of prevs position when maze is not built?
         //if(currentpos.color==1)
         //{
-        includeY.color = 1;
-        //currentpos.color + 1;
-        includeY.inclusion = true;
+        includeBY.src = 5;
+        includeBY.target = 1;
+        includeBY.type = 8;
+        //create more of these
+        includeBY.inclusion = true; //not using this anywhere
 
-        
-        excludeR.color = 1 | 2 | 4 | 5 | 6;
+        excludeR.src = 3;
+        excludeR.target = 1 | 2 | 4 | 5 | 6;
         excludeR.inclusion = false;
+        excludeR.type = 9;
         //add to the list
-   
-        excludeO.color = 1 | 3 | 4 | 5 | 6;
+
+        excludeO.src = 2;
+        excludeO.target = 1 | 3 | 4 | 5 | 6;
         excludeO.inclusion = false;
+        excludeO.type = 9;
 
-     
-        excludeY.color = 3 | 2 | 4 | 5 | 6;
+        excludeY.src = 1;
+        excludeY.target = 3 | 2 | 4 | 5 | 6;
         excludeY.inclusion = false;
+        excludeY.type = 9;
 
-        
-        excludeB.color = 1 | 2 | 4 | 3 | 6;
+
+        excludeB.src = 5;
+        excludeB.target = 1 | 2 | 4 | 3 | 6;
         excludeB.inclusion = false;
+        excludeB.type = 9;
 
-        
-        excludeG.color = 1 | 2 | 3 | 5 | 6;
+        excludeG.src = 4;
+        excludeG.target = 1 | 2 | 3 | 5 | 6;
         excludeG.inclusion = false;
+        excludeG.type = 9;
 
-      
-        excludeP.color = 1 | 2 | 4 | 5 | 3;
+
+        excludeP.src = 6;
+        excludeP.target = 1 | 2 | 4 | 5 | 3;
         excludeP.inclusion = false;
+        excludeP.type = 9;
 
-        ColorRules block; //block is the same as exclude C if we dont keep track of prevs position
+        blockY.target=2 | 3 | 4 | 5 | 6;
+        blockY.type = 10;
+
+        blockO.target = 1 | 3 | 4 | 5 | 6;
+        blockO.type = 10;
+
+        blockR.target = 1 | 2 | 4 | 5 | 6;
+        blockR.type = 10;
+
+        blockG.target= 1 | 2 | 3 | 5 | 6;
+        blockG.type = 10;
+
+        blockB.target= 1 | 2 | 3 | 4 | 6;
+        blockB.type = 10;
+
+        blockP.target = 1 | 2 | 3 | 4 | 5; //src and inclusion not defined yet
+        blockP.type = 10;
+
 
     }
 
-    public void fitnessOne(List<MovementRules> m, List<ColorRules> c) 
-    {
-        int w1, w2;
 
-        for (int i = 0; i < m.Count; i++)
+    public void crossover(MovementRules m, ColorRules c)
+    {
+        System.Random randNum = new System.Random();
+
+
+        // List<object> c1 = new List<object>();
+        //List<object> c2 = new List<object>();
+        //List<object> allList = movementRuleSets.Cast<object>().Concat(colorRuleSets).ToList();
+
+        //int[] c1 = new int[7];
+        //int[] c2 = new int[7]; //7 rules each time
+
+        List<int> c1 = new List<int>(7); //just adding the types to chromosomes
+        List<int> c2 = new List<int>(7);
+        List<int> allList = new List<int>();
+
+        foreach (var item in movementRuleSets)
         {
-            w1 = m[i].w;
-            w2 = c[i].w;
-            
-           
+            allList.Add(item.type);
+        }
+        foreach (var item in colorRuleSets)
+        {
+            allList.Add(item.type);
         }
 
+        
+        List<int> usedIdx1 = new List<int>();
+        List<int> usedIdx2 = new List<int>();
+
+
+        for (int i = 0; i < c1.Count; i++)   //how long do we want each chromosome to be? a subset of allList
+        {
+            int idx1 = randNum.Next(0,allList.Count); 
+            usedIdx1.Add(idx1); //avoiding repeated rules
+
+            if (!usedIdx1.Contains(idx1))
+            {
+                c1.Add(allList[idx1]);
+            }
+        }
+
+        for (int i = 0; i < c2.Count; i++)   //how long do we want each chromosome to be? a subset of allList
+        {
+            int idx2 = randNum.Next(0, allList.Count);
+            usedIdx2.Add(idx2); //avoiding repeated rules
+
+            if (!usedIdx2.Contains(idx2))
+            {
+                c2.Add(allList[idx2]);
+            }
+        }
+      
+        mutate(c1, c2);
+        
+
+
+    }
+
+    public void mutate(List<int> c1, List<int> c2)
+    {
+        int[] chromosomes = new int[c1.Count + c2.Count]; //array containing all types
+        for (int i = 0; i < c1.Count; i++)
+        {
+            chromosomes[i] = c1[i]; //should be c1.type but type isnt working for c1 :/
+        }
+        for (int i = c1.Count + 1; i < c1.Count + c2.Count; i++)
+        {
+            chromosomes[i] = c2[i]; //
+        }
+        int f = fitnessOne(chromosomes);
+    }
+
+
+
+    public int fitnessOne(int[] c)
+    {
+        //chromosome that has more different types of rules is a better fit rule.
+
+        //fitness metrics : 1)variation in types , 2)too much or too less of only color rules/movement rules
+
         //fitness = [chromosome[1] for chromosome in population ]
+        int fit = 1;
+        
+        int t=0;
+        int[] uniqueTypes = new int[11]; //rule types , not using index 0, want to use index 1-10
+        for (int i = 0; i < c.Length; i++) //check the variation in types
+        {
+            t = c[i];
+            uniqueTypes[t]++; //incrementing the rule types using rule types as index
+        }
 
+        for (int i = 0; i < uniqueTypes.Length; i++)
+        {
+            if (uniqueTypes[i] != 0)
+            {
+                fit = fit * uniqueTypes[i];
+            }
+        }
+        print("fitness 1 : all rule types are used only once");
+        print("fitness value 1-2 is good. Higher value means same type of rule is being repeated more than twice, which is not good.");
+        print("fitness value is " + fit);
 
-
-
+        if (fit <= 2)
+        {
+            //pass c1,c2 to maze
+        }
+        else
+        {
+            crossover(movementRuleSets, colorRuleSets);
+        }
+        return fit;
+    }
 }
-
 
 
 
