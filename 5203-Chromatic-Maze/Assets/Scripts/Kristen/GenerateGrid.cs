4aed3c678cd7933033341ce35598bcee79c0530b
Rifah Sama Aziz <rifahz999@gmail.com>
2021-11-24 09:50:52 -0500

    corrected generategrid color

--- 5203-Chromatic-Maze/Assets/Scripts/Kristen/GenerateGrid.cs
+++ 5203-Chromatic-Maze/Assets/Scripts/Kristen/GenerateGrid.cs
@@ -75,7 +75,7 @@ public class GenerateGrid : MonoBehaviour
                 //TEMPORARY - DELETE LATER
                 Material mat = (Material)colours[Random.Range(0, colours.Length)];
                 tile.GetComponent<SpriteRenderer>().material.shader = mat.shader;
-                tile.GetComponent<SpriteRenderer>().material.colour = mat.colour;
+                tile.GetComponent<SpriteRenderer>().material.color = mat.color;
 
                 tiles[counter] = tile;
 
