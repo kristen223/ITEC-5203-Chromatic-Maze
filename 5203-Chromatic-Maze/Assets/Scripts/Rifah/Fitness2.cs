f12ffb452c2bf9f14bec62f0d5a45a16005a5901
Kristen <kristen_grinyer@icloud.com>
2021-12-11 16:01:08 -0500

    updated Maze Creation class

--- 5203-Chromatic-Maze/Assets/Scripts/Rifah/Fitness2.cs
+++ 5203-Chromatic-Maze/Assets/Scripts/Rifah/Fitness2.cs
@@ -26,6 +26,6 @@ public class Fitness2 : MonoBehaviour
 
     public static void fitness2(ColourAssigner.ColouredMaze colouredMaze)
     {
-        TraverseMaze.SolutionPaths sPaths = ColourAssigner.paths; //a new struct I made, gives u the length of the longest and shortest path and # of paths
+        
     }
 }
--- 5203-Chromatic-Maze/Assets/Scripts/Rifah/MazeCreation.cs
+++ 5203-Chromatic-Maze/Assets/Scripts/Rifah/MazeCreation.cs
@@ -5,24 +5,91 @@ using UnityEngine;
 public class MazeCreation : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject mazePrefab; //put this at the top
+    private static GameObject mazePrefab; //put this at the top
+    private static List<GameObject> prefabs = new List<GameObject>(); //list of colour assigner prefabs
+    private static List<ColourAssigner.ColouredMaze> cmazes = new List<ColourAssigner.ColouredMaze>(); //list of coloured mazes, one for each chromosome
 
     void Start()
     {
         mazePrefab = (GameObject)Resources.Load("ColourAssigner"); //put this in Start method
     }
 
+    //Creates a coloured maze for each final chromosome
+    public static void CreateCMazes(ArrayList chromosomes, Dictionary<int, Dictionary<int, Type>> clist, List<MovementRule> m, List<ColourRule> c)   //an arraylist of chromosomes. its like : chromosomes=>[mr,cr,mr,cr,mr,cr,mr,cr.....]
+    {
+
+        foreach (Dictionary<int, int> chromosome in chromosomes) //for each chromosome in final list of chromosomes
+        {
+            List<MovementRule> mr = new List<MovementRule>();
+            List<ColourRule> cr = new List<ColourRule>();
+            //GetFinalRules code
+            foreach (KeyValuePair<int, int> k in chromosome) //for each rule in chromosome
+            {
+                if (clist.ContainsKey(k.Key)) //chosenChr.key = clist.key
+                {
+                    foreach (Dictionary<int, Type> d in clist.Values)
+                    {
+                        foreach (KeyValuePair<int, Type> kvp in d)
+                        {
+                            if (kvp.Value == Type.exclude || kvp.Value == Type.include)
+                            {
+                                cr.Add(Fitness1.GetCRule(kvp.Key, c));
+                            }
+                            else
+                            {
+                                mr.Add(Fitness1.GetMRule(kvp.Key, m));
+                            }
+                        }
+                    }
+                }
+
+                GameObject maze = Instantiate(mazePrefab, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+                prefabs.Add(maze);
+
+                maze.GetComponent<ColourAssigner>().SetRules(mr, cr); //set the maze rules to the current chromosome's rules
+                cmazes.Add(maze.GetComponent<ColourAssigner>().ColourMaze()); //colour the maze and add it to the list
+            }
+        }
+
+
+        /*
+        
+        List of coloured mazes now exists
+        1.  Call fitness 2 here (with list of coloured mazes and prefabs a parameter) and return a single coloured maze
+            **inside fitness two, once final maze is picked, delete all other prefabs and coloured mazes from lists
+
+            CODE: ColourAssigner.ColouredMaze finalMaze = fitnessc(cmazes, prefabs);
+
+
+
+        2. Set Tile components of tile game objects in grid
+
+            for (int i = 0; i < GenerateGrid.tiles.Length; i++)
+            {
+                Component chosenComponent = finalMaze.maze.tiles[i].GetComponent<Tile>();
+
+                System.Type type = chosenComponent.GetType();
+
+                System.Reflection.FieldInfo[] fields = type.GetFields();
+                foreach (System.Reflection.FieldInfo field in fields)
+                {
+                    field.SetValue(GenerateGrid.tiles[i].GetComponent<Tile>(), field.GetValue(chosenComponent));
+                }
+            }
+
+        3. Set all of the player controller variables and texts according to finalMaze
+
+        */
+    }
+
+
 
     public static void getFinalRules(Dictionary<int, int> chosenChr, Dictionary<int, Dictionary<int, Type>> clist, List<MovementRule> m, List<ColourRule> c)
     {
         //int[][] chromosomes = new int[2][];
 
-
-        
         foreach (KeyValuePair<int, int> k in chosenChr)
         {
-
-
 
             List<MovementRule> mr = new List<MovementRule>();
             List<ColourRule> cr = new List<ColourRule>();
@@ -41,71 +108,14 @@ public class MazeCreation : MonoBehaviour
                             mr.Add(Fitness1.GetMRule(kvp.Key, m));
                         }
                         // ChosenRulesIdx.Add(kvp.Key);
-
-
                     }
                 }
-
             }
 
-            //CALL SETRULES HERE , have mr,cr
-            setrules(mr, cr);
             //chromosomes.Add(mr);
             //chromosomes.Add(cr);
-
         }
         //MazeCreation.mazeC(chromosomes);
-
-    }
-
-
-
-
-
-    //add this once you have final list of chromosomes
-    //create list of prefabs and their coloured mazes for each chromosome
-
-    public static void mazeC(ArrayList chromosomes)   //an arraylist of chromosomes. its like : chromosomes=>[mr,cr,mr,cr,mr,cr,mr,cr.....]
-    {
-        List<GameObject> prefabs = new List<GameObject>();
-        List<ColourAssigner.ColouredMaze> cmazes = new List<ColourAssigner.ColouredMaze>(); //list of coloured mazes, one for each chromosome
-        foreach (var item in chromosomes)
-        {
-            GameObject maze = Instantiate(mazePrefab, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
-            prefabs.Add(maze);
-
-
-
-            List<MovementRule> mr = new List<MovementRule>();
-            List<ColourRule> cr = new List<ColourRule>();
-            
-
-            maze.GetComponent<ColourAssigner>().SetRules(mr, cr); //set the maze rules to this chromosome's rules
-            cmazes.Add(maze.GetComponent<ColourAssigner>().ColourMaze()); //colour the maze and add it to the list
-        }
-
-        //compare mazes and pick one (fitness 2)
-
-        //delete all prefabs except the chosen one
-
-        //temp variable to represent the chosen maze
-        ColourAssigner.ColouredMaze chosen = new ColourAssigner.ColouredMaze();
-
-        //set Tile components of tile game objects in grid
-        for (int i = 0; i < GenerateGrid.tiles.Length; i++)
-        {
-            Component chosenComponent = chosen.maze.tiles[i].GetComponent<Tile>();
-
-            System.Type type = chosenComponent.GetType();
-
-            System.Reflection.FieldInfo[] fields = type.GetFields();
-            foreach (System.Reflection.FieldInfo field in fields)
-            {
-                field.SetValue(GenerateGrid.tiles[i].GetComponent<Tile>(), field.GetValue(chosenComponent));
-            }
-        }
-
-        //Set all of the player controller variables and texts here (I’ll do this)
     }
 }
    
